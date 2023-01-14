9eb6967f8c00efe5664840450360a806a84daf02
penguinboots <sabrina.ynw@gmail.com>
2023-01-14 11:22:12 -0800

    add cookieSession name/keys

--- express_server.js
+++ express_server.js
@@ -12,7 +12,10 @@ app.set("view engine", "ejs");
 ////////////////
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieSession());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["keykeykey"]
+}));
 
 /////////////
 /* HELPERS */
