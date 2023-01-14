a64cf802801910a4b26bd8a0c2958d767c1d215f
penguinboots <sabrina.ynw@gmail.com>
2023-01-14 10:28:54 -0800

    refactor urlDatabase

--- express_server.js
+++ express_server.js
@@ -36,8 +36,14 @@ const getUserByEmail = (email) => {
 ///////////////
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "sd3dss"
+  },
+  ssm5xK: {
+    longURL: "http://www.google.com",
+    userID: "sd3dss"
+  }
 };
 
 const userDatabase = {
